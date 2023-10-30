a71a6e58d727af815e9ab11ddf1a1e8fe993030a
Federico Sabiche <fsabiche>
2023-10-30 16:47:39 -0300

    registro de autentificacion hecho en front

--- front/src/pages/RegisterPage.jsx
+++ front/src/pages/RegisterPage.jsx
@@ -1,38 +1,60 @@
-import { useForm } from "react-hook-form"; //se utiliza para
-import { registerRequest } from "../api/auth";
+import { useForm } from "react-hook-form";
+import { useAuth } from "../context/AuthContext";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function RegisterPage() {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const { signUp, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
 
-  const onSubmit = handleSubmit(async (values) => {
+  useEffect(() => {
+    if (isAuthenticated) navigate("/login"); //se puso login para que se redireccione a algun lado pero hay que cambiarlo, en el tutorial lo envia a task porque es un task list
+  }, [isAuthenticated]);
+
+  const onSubmit = async (values) => {
     console.log(values);
-    const res = await registerRequest(values);
-    console.log(res);
-  });
+    signUp(values);
+  };
+
   return (
     <div className="bg-zinc-800 max-w-md p-10 rounded-md">
-      <form onSubmit={onSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <input
           type="text"
-          {...register("username", { require: true })}
+          {...register("username", { required: true })}
           className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
           placeholder="Username"
         />
+        {errors.username && (
+          <p className="text-red-500">Username is required</p>
+        )}
         <input
           type="email"
-          {...register("email", { require: true })}
+          {...register("email", { required: true })}
           className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
           placeholder="Email"
         />
+        {errors.email && (
+          <p className="text-red-500">Email is required</p>
+        )}
         <input
           type="password"
-          {...register("password", { require: true })}
+          {...register("password", { required: true })}
           className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
           placeholder="Password"
         />
+        {errors.password && (
+          <p className="text-red-500">Password is required</p>
+        )}
         <button type="submit">Register</button>
       </form>
     </div>
   );
 }
+
 export default RegisterPage;
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -9,6 +9,10 @@ export const register = async (req, res) => {
   console.log(email, password, username);
 
   try {
+    const userFound = await User.findOne({ email });
+    if (userFound)
+      return res.status(400).json({ message: ['the email is ready in use'] });
+
     const passwordHash = await bcrypt.hash(password, 10); //encryptamos el password
 
     const newUser = new User({
@@ -38,12 +42,12 @@ export const login = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-
-    const userFound = await User.findOne({email})
-    if (!userFound) return res.status(400).json({ message: "User not found" })
+    const userFound = await User.findOne({ email });
+    if (!userFound) return res.status(400).json({ message: "User not found" });
 
     const isMatch = await bcrypt.compare(password, userFound.password); //comparamos el password
-    if(!isMatch) return res.status(400).json({ message: "Incorrect password" })
+    if (!isMatch)
+      return res.status(400).json({ message: "Incorrect password" });
 
     const token = await createAccessToken({ id: userFound._id });
 
@@ -60,24 +64,23 @@ export const login = async (req, res) => {
   }
 };
 // logout
-export const logout = (req,res) =>{
-
+export const logout = (req, res) => {
   res.cookie("token", "", {
-    expires: new Date(0)
-  })
-  return res.sendStatus(200)
-}
+    expires: new Date(0),
+  });
+  return res.sendStatus(200);
+};
 // profile
-export const profile = async (req,res) =>{
-const userFound = await User.findById(req.user.id)
+export const profile = async (req, res) => {
+  const userFound = await User.findById(req.user.id);
 
-if(!userFound) return res.status(400).json({ message: "User not found" })
+  if (!userFound) return res.status(400).json({ message: "User not found" });
 
-return res.json({
-  id: userFound._id,
-  username: userFound.username,
-  email: userFound.email,
-  createdAt: userFound.createdAt,
-  updatedAt: userFound.updatedAt,
-})
-}
+  return res.json({
+    id: userFound._id,
+    username: userFound.username,
+    email: userFound.email,
+    createdAt: userFound.createdAt,
+    updatedAt: userFound.updatedAt,
+  });
+};
